cc95a955c1a484b31f4675c70729103731857265
ad56313217 <645864919@qq.com>
2019-07-01 11:44:19 +0800

    add login

---
+++ Client/Source/ChessCards/UI/CCG_LoginPage.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CCG_LoginPage.h"
+#include "Button.h"
+/**³õÊ¼»¯*/
+bool UCCG_LoginPage::Initialize()
+{
+	bool initRtn = Super::Initialize();
+	if (!initRtn)
+	{
+		return false;
+	}
+	return true;
+}
+
+void UCCG_LoginPage::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	AddEventListening();
+}
+
+void UCCG_LoginPage::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+}
+
+void UCCG_LoginPage::AddEventListening()
+{
+	//if (StartButton)
+	//{
+	//	StartButton->OnClicked.AddDynamic(this, &UCCG_MainPage::Start_Btf);
+	//}
+}
---
+++ Client/Source/ChessCards/UI/CCG_LoginPage.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/CCG_PageBase.h"
+#include "CCG_LoginPage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHESSCARDS_API UCCG_LoginPage : public UCCG_PageBase
+{
+	GENERATED_BODY()
+public:
+	/**初始化*/
+	virtual bool Initialize() override;
+
+protected:
+	//游戏开始入口
+	virtual void NativeConstruct();
+	//主循环
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+	//监听事件
+	UFUNCTION()
+		void AddEventListening();
+};
